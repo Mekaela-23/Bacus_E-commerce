3fc822806c60ff98ca8ae91d3b9a1aeb2e446460
Mekaela-23 <mekaelakristinemaebadarbacus@gmail.com>
2026-04-19 22:08:52 +0800

    luminaTEST1

--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,6 @@
 export default function swDev()
 {
-    let swUrl = `${process.env.PUBLIC_URL}sw.js`;
+    let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
     navigator.serviceWorker.register(swUrl).then((response) => {
         console.warn("Service Worker Registered");
     }).catch((error) => {
